cc9c1fa730912efbb8223fde70f84c1c9200d3ab
harisismail69 <u2022201@giki.edu.pk>
2024-05-04 16:52:00 +0500

    Initial Commit

---
+++ Data/AppDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models.Entity;
+
+namespace WebApplication2.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> students { get; set; }
+    }
+}
---
+++ Migrations/20240503212017_Initial Migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    subscribed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
